628c5b5b9b96a20c032d7b66e8bc5663d8ae7541
samdeanefox <samdeanefox@gmail.com>
2016-04-26 23:05:39 -0700

    added deciml

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var display: UILabel!
     
     var userIsInTheMiddleOfTypingANumber = false
+    var decimalEntered = false
+    var brain = CalculatorBrains()
     
     @IBAction func appendDigit(sender: UIButton) {
         let digit = sender.currentTitle!
@@ -24,46 +26,40 @@ class ViewController: UIViewController {
             userIsInTheMiddleOfTypingANumber = true
         }
     }
+    @IBAction func appendDecimal() {
+        if userIsInTheMiddleOfTypingANumber && !decimalEntered {
+            decimalEntered = true
+            display.text = display.text! + "."
+        }
+    }
     
     @IBAction func operate(sender: UIButton) {
         let operation = sender.currentTitle!
         if userIsInTheMiddleOfTypingANumber {
             enter()
         }
-        switch operation {
-            case "×": performOperation { $0 * $1 }
-            case "÷": performOperation { $1 / $0 }
-            case "+": performOperation { $0 + $1 }
-            case "−": performOperation { $1 - $0 }
-            case "√": root { sqrt($0) }
-            default: break
+        if let operation = sender.currentTitle {
+            if let result = brain.performOperation(operation) {
+                displayValue = result
+            } else {
+                displayValue = 0
+            }
         }
     }
-    
-    func performOperation(operation: (Double, Double) -> Double) {
-        if operandStack.count >= 2 {
-            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
-            enter()
-        }
-    }
-    
-    func root(operation: Double -> Double) {
-        if operandStack.count >= 1 {
-            displayValue = operation(operandStack.removeLast())
-            enter()
-        }
-    }
-    
-    var operandStack = Array<Double>()
     
     @IBAction func enter() {
         userIsInTheMiddleOfTypingANumber = false
-        operandStack.append(displayValue)
-        println("operadStack = \(operandStack)")
+        decimalEntered = false
+        if let result = brain.pushOperand(displayValue) {
+            displayValue = result
+        } else {
+            displayValue = 0
+        }
     }
     
     var displayValue: Double {
         get {
+            println("\(NSNumberFormatter().numberFromString(display.text!)!.doubleValue)")
             return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
         }
         set {
